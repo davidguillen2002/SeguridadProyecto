4d0dda4b1b19ab9d5f08963e5b72bda12aa4f334
davidguillen2002 <davidanguillen@gmail.com>
2024-06-10 16:19:31 -0500

    CAMBIO 12

--- SeguridadProyecto/SeguridadProyecto/urls.py
+++ SeguridadProyecto/SeguridadProyecto/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 # SeguridadProyecto/urls.py
 from django.urls import path, include
-from two_factor.urls import urlpatterns as tf_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('accounts/', include('allauth.urls')),
     path('', include(tf_urls)),
